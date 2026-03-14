86e28c8e762154a208c0a9a0763ecb7bf327c690
dunamis0319 <47356779+dunamis0319@users.noreply.github.com>
2026-03-15 06:26:34 +0900

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: "/-warizan-master",
+  assetPrefix: "/-warizan-master/",
 };
 
 export default nextConfig;
